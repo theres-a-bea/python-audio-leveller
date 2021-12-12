94a84dc6fea174a3770da90c4cef2e9997229996
theres.a.bea <samuelsson.bea@gmail.com>
2021-12-12 10:22:25 +0800

    Improved documentation in code

--- Modules.py
+++ Modules.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 import os
 import eyed3
-import time
 from datetime import datetime
 
+#Converts input string into parsable by URL/path
 def safeconvert(input, type):
+    #if a URL, removes broken chars and replaces with URL safe ones
     if type == "url":
         output=input.replace(" ","-")
         output=output.replace(',','')
@@ -12,6 +13,8 @@ def safeconvert(input, type):
         output=output.replace(')','')
         output=output.encode('ascii', 'xmlcharrefreplace')
         output=output.decode('ascii')
+    
+    #if a path, removes bash breaking characters
     if type == "path":
         output=input.replace(" ",r"\ ")
         output=output.replace("(",r"\(")
@@ -20,54 +23,86 @@ def safeconvert(input, type):
         output=output.replace("&",r"\&")
         output=output.replace("~",r"\~")
         output=output.replace("'",r"\'")
+    
+    #if unknown, defaults to removing everything
     else:
-        output=input.replace(" ","-")
-        output=output.replace(',','')
-        output=output.replace('(','')
-        output=output.replace(')','')
-
+        output=input.replace(" ","")
+        output=output.replace(',',"")
+        output=output.replace('(',"")
+        output=output.replace(')',"")
+        output=output.replace("*","")
+        output=output.replace("&","")
+        output=output.replace("~","")
+        output=output.replace("'","")
         output=output.encode('ascii', 'xmlcharrefreplace')
         output=output.decode('ascii')
     return output
 
 #Gets visable files in directory, returns fileList in array as posixPathes
 def getFilesInDir(dir):
+    #checks for all files that are .mp3,.wav,.aac or .m4a
     fileList = list(Path(dir).rglob("*.mp3"))
     fileList.append(list(Path(dir).rglob("*.wav")))
+    fileList.append(list(Path(dir).rglob("*.aac")))
     fileList.append(list(Path(dir).rglob("*.m4a")))
     return fileList
 
+#Generates Timestamp
 def timestamp():
     dateTimeObj = datetime.now()
     timecode = dateTimeObj.strftime("%Y-%m-%d_%H%M%S")
     return timecode
 
 #Applies ffmpeg's loudnorm
-def leveller(inputfile, outputfile):
+def leveller(inputfile, outputfile, intLoud=-9, LRA=11, TP=-1.5):
     print("Processing " + str(inputfile))
+
+    #Converts input strings into bash-safe pathes
     safein = safeconvert(inputfile, "path")
     safeout = safeconvert(outputfile, "path")
-    command = "ffmpeg -loglevel error -i " + str(safein) + " -af loudnorm=I=-9:LRA=11:TP=-1.5 " + str(safeout)
+
+    #Defines Loudnorm variables:
+
+    integratedLoudness=str(intLoud)
+    #Loudness Target, effectively LUFS
+
+    loudnessRange=str(LRA)
+    #Difference between peak and trough
+
+    truePeak=str(TP)
+    #True peak of the file
+
+
+    #Generates & runs ffmpeg command
+    command = "ffmpeg -loglevel error -i " + str(safein) + " -af loudnorm=I="+integratedLoudness+":LRA="+loudnessRange+":TP="+truePeak+" " + str(safeout)
+    
+
     print("Running " + command)
     os.system(command)
 
+#Pulls meta from inputfile, writes to output file
 def metaMatch(inputfile, outputfile):
-
     print ('Writing meta to ' + outputfile)
+    
+    #loads working files
     inmeta = eyed3.load(inputfile)
     outmeta = eyed3.load(outputfile)
+
     arname = inmeta.tag.artist
     outmeta.tag.artist = arname
     print ('Artist: ' + arname)
+
     trname = inmeta.tag.title
     outmeta.tag.title = trname
     print ('Track: ' + trname)
+
     alname = inmeta.tag.album
     outmeta.tag.album = alname
     print ('Album: ' + alname)
+
     trnum = inmeta.tag.track_num
     outmeta.tag.track_num = trnum[0]
     print ('Track Number: ' + str(trnum[0]))
+
     outmeta.tag.save()
     print ('Meta Saved.')
-
--- main.py
+++ main.py
@@ -8,8 +8,11 @@ fileArray = Modules.getFilesInDir(inputdir)
 
 
 for file in fileArray:
+    #splits input file into name and extension
     inname, ext = os.path.splitext(file)
+    #generates tmp file with same ext
     tmpfile = "foo" + ext
+    #finds directory of input file and changes directory to it (look, it makes ffmpeg cry less, fuck off)
     folder = os.path.dirname(file)
     print("Changing Directory to " + folder)
     startwd = os.getcwd()
@@ -17,12 +20,14 @@ for file in fileArray:
 
     try:
         permout = os.path.basename(file)
+        #once we have the basename of the file, run the leveller and metamatcher
         Modules.leveller(permout,tmpfile)
         Modules.metaMatch(permout,tmpfile)
         print("Renaming file")
         os.rename(tmpfile,permout)
 
     except:
+        #if above fails, log for investigation
         print("Exception found - logging")
         os.chdir(startwd)
         stamp = Modules.timestamp()
@@ -32,4 +37,6 @@ for file in fileArray:
         f.write(exception)
         f.close()
 
-    os.chdir(startwd)
+    #if we're not in the main dir, change back to it
+    if os.getcwd() != startwd:
+        os.chdir(startwd)
